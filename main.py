8a52a159be06d9fddd89b125487a2a407d1fc87a
Alina Bondarets <alinium3012@gmail.com>
2021-02-23 17:39:19 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+import requests
+import json
+
+from pprint import pprint
+
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    username = request.form.get("username")
+    token = request.form.get("token")
+    if not username or not token:
+        return render_template("failure.html")
+    return render_template("success.html")
+
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAD4uNAEAAAAACMARcBiqeRczBEyrTUmCBBcahlM%3DjwmyjufJqiajtYee3mEYsF2LEjSz5CVnnA5PtdF8ey1kk1Jb0S'
+
+def find_users_friends_info(bearer_token: str, user_name: str):
+    base_url = 'https://api.twitter.com/'
+
+    search_url = '{}1.1/friends/list.json'.format(base_url)
+
+    search_headers = {
+        'Authorization': 'Bearer {}'.format(bearer_token)
+    }
+
+    search_params = {
+        'screen_name': '@BarackObama',
+        'count': 2
+    }
+
+    response = requests.get(search_url, headers=search_headers, params=search_params)
+    print(response)
+
+    json_response = response.json()
+    pprint(json_response)
+
+
+# if __name__ == '__main__':
+#     app.debug = True
+#     app.run()
