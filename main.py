2e4fa2e40af91410ea0f6099f9bf552c846e7e83
Alina Bondarets <alinium3012@gmail.com>
2021-02-23 22:10:54 +0200

    Add side cases support

--- main.py
+++ main.py
@@ -2,26 +2,20 @@ import requests
 import json
 import folium
 
+from flask import Flask, redirect, render_template, request
 from folium.plugins import MarkerCluster
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
 from geopy.exc import GeocoderUnavailable
-from flask import Flask, redirect, render_template, request
 
 
-def find_users_friends_info(bearer_token: str, user_name: str):
+def find_users_friends_info(bearer_token: str, user_name: str) -> dict:
     base_url = 'https://api.twitter.com/'
-
     search_url = '{}1.1/friends/list.json'.format(base_url)
 
-    search_headers = {
-        'Authorization': 'Bearer {}'.format(bearer_token)
-    }
-
-    search_params = {
-        'screen_name': user_name,
-        'count': 15
-    }
+    search_headers = {'Authorization': 'Bearer {}'.format(bearer_token)}
+    search_params = {'screen_name': user_name,
+                     'count': 15}
 
     response = requests.get(
         search_url, headers=search_headers, params=search_params)
@@ -30,7 +24,7 @@ def find_users_friends_info(bearer_token: str, user_name: str):
     return json_response
 
 
-def find_friends_locations(friends_dict: dict):
+def find_friends_locations(friends_dict: dict) -> dict:
     locations_dict = {}
     for user_info in friends_dict['users']:
         user_name = []
@@ -64,10 +58,9 @@ def create_html_map(coordinates_dict: dict):
 
     for tup in coordinates_dict.keys():
         folium.Marker(location=[tup[0], tup[1]],
-                      popup=f'Name: «{coordinates_dict[tup][0]}»;\nUsername: «{coordinates_dict[tup][1]}»',
-                      icon=folium.Icon(color='darkpurple', icon='user')).add_to(friends_location)
+            popup=f'Name: «{coordinates_dict[tup][0]}»;\nUsername: «{coordinates_dict[tup][1]}»',
+            icon=folium.Icon(color='darkpurple', icon='user')).add_to(friends_location)
     map.add_child(fg)
-    map.save('Your_map_here.html')
     return map._repr_html_()
 
 
@@ -85,16 +78,22 @@ def register():
     token = request.form.get("token")
     if not username or not token:
         return render_template("failure.html")
+
     friends_dict = find_users_friends_info(token, username)
+    if friends_dict == {'errors': [{'code': 34, 'message': 'Sorry, that page does not exist.'}]}:
+        return render_template("username_fail.html")
+    elif friends_dict == {'users': [], 'next_cursor': 0, 'next_cursor_str': '0',
+                          'previous_cursor': 0, 'previous_cursor_str': '0', 'total_count': None}:
+        return render_template("no_locations.html")
+
     locations_dict = find_friends_locations(friends_dict)
     coordinates_dict = find_coordinates(locations_dict)
     return create_html_map(coordinates_dict)
 
 
 if __name__ == '__main__':
-    geolocator = Nominatim(user_agent="my")
+    geolocator = Nominatim(user_agent="friends_map")
     geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
 
     app.debug = True
     app.run()
-
